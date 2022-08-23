da84e5258f73b6819de9a12130f05020f44e4d22
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 17:53:51 +0300

    Task01 done

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] CreateMatrix(int lenght, int width)
+{
+    double[,] matrix = new double[lenght, width];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = random.NextDouble();
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        Console.WriteLine(" ");
+    }
+
+}
+double[,] matrix = CreateMatrix(3, 3);
+PrintMatrix(matrix);
