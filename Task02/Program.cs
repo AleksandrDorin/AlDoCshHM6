ccbf85b712148431e868f3dacbfcc6e391323a2b
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 18:07:23 +0300

    Task02 done

---
+++ Task02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] CreateMatrix(int length, int width)
+{
+    int[,] matrix = new int[length, width];
+    Random random = new Random();
+    for(int i=0;i<matrix.GetLength(0);i++)
+        for(int j=0;j<matrix.GetLength(1);j++)
+            matrix[i,j] = i + j;
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i=0;i<matrix.GetLength(0);i++)
+    {
+        for(int j=0;j<matrix.GetLength(1);j++)
+            Console.Write($"{matrix[i,j]} \t");
+        Console.WriteLine();
+    }
+    
+}
+int[,] matrix = CreateMatrix(6,6);
+PrintMatrix(matrix);
